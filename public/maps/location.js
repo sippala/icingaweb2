32e634a0f38b7d95e106cca532f7a680249f7ab9
Shankar Ippala <ishan753@gmail.com>
2017-05-18 15:26:31 -0400

    Add files via upload

---
+++ public/maps/location.js
@@ -0,0 +1,29 @@
+
+var map = L.map( 'map', {
+  center: [40.4322, 116.57],
+  minZoom: 2,
+  zoom: 11
+})
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo( map )
+
+var myURL = jQuery( 'script[src$="location.js"]' ).attr( 'src' ).replace( 'location.js', '' )
+
+var myIcon = L.icon({
+  //iconUrl: myURL + 'images/pin24.png',
+  iconUrl: myURL + 'res/remote.ico',
+  //iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+})
+
+for ( var i=0; i < markers.data.length; ++i )
+{
+ L.marker( [markers.data[i].Lat, markers.data[i].Lon], {icon: myIcon} )
+  .bindPopup( '<a href="' + markers.data[i].url + '" target="_blank">' + markers.data[i].Name + '</a>' )
+  .addTo( map );
+}
